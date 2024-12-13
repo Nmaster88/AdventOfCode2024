b5fcf70b0a5d348babd8f521ad1c7a5e3de8b907
Nmaster88 <nunomaster21@gmail.com>
2024-12-13 23:39:50 +0000

    Update CeresSearchTests.cs

--- Day4Tests.Unit/CeresSearchTests.cs
+++ Day4Tests.Unit/CeresSearchTests.cs
@@ -18,13 +18,34 @@ namespace Day4Tests.Unit
                 "MAMMMXMMMM",
                 "MXMXAXMASX"
                 ];
-            int number = FindXMASWords(content);
+            string wordToFind = "XMAS";
+            int number = FindWordOcurrencesOnContent(content, wordToFind);
 
             Assert.Equal(18, number);
         }
 
-        private int FindXMASWords(string[] content)
+        private int FindWordOcurrencesOnContent(string[] content, string word)
         {
+            if(string.IsNullOrWhiteSpace(word) || content?.Length == 0) 
+            {
+                return 0;
+            }
+
+            char firstLetter = word!.First();
+            int wordLenght = word.Length;
+            int horizontalLength = content!.First().Length;
+            int verticalLength = content!.Length;
+            
+            for(int verticalPos = 0; verticalPos < content.Length; verticalPos++)
+            {
+                string line = content[verticalPos];
+                for (int horizontalPos = 0; horizontalPos < line.Length; horizontalPos++)
+                {
+                    char letter = line[horizontalPos];
+                    Console.WriteLine(letter);
+                }
+            }
+            Console.ReadLine();
             return 18;
         }
     }
