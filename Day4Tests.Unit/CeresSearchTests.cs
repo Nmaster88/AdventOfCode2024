5f4a8ff2670c9815f53c4023d2f674f3be1694bf
Nmaster88 <nunomaster21@gmail.com>
2024-12-14 23:56:50 +0000

    Update CeresSearchTests.cs

--- Day4Tests.Unit/CeresSearchTests.cs
+++ Day4Tests.Unit/CeresSearchTests.cs
@@ -6,7 +6,7 @@ namespace Day4Tests.Unit
         [Fact]
         public void GivenTheFollowingTextLines_WhenTheXMASWordsAreFind_ThenItReturnsTheRightNumber()
         {
-            string[] content = [
+            string[] contentInput = [
                 "MMMSXXMASM",
                 "MSAMXMSMSA",
                 "AMXSXMAAMM",
@@ -18,33 +18,74 @@ namespace Day4Tests.Unit
                 "MAMMMXMMMM",
                 "MXMXAXMASX"
                 ];
-            string wordToFind = "XMAS";
+
+            content = new char[contentInput.Length, contentInput[0].Length];
+
+            for (int verticalPos = 0; verticalPos < contentInput.Length; verticalPos++)
+            {
+                for (int horizontalPos = 0; horizontalPos < contentInput[verticalPos].Length; horizontalPos++)
+                {
+                    content[horizontalPos, verticalPos] = contentInput[verticalPos][horizontalPos];
+                }
+            }
+
+                    string wordToFind = "XMAS";
             int number = FindWordOcurrencesOnContent(content, wordToFind);
 
             Assert.Equal(18, number);
         }
 
-        private int FindWordOcurrencesOnContent(string[] content, string word)
+        private char[,] content;
+        private char[,] coordMatrix;
+
+        private int FindWordOcurrencesOnContent(char[,] content, string word)
         {
             if(string.IsNullOrWhiteSpace(word) || content?.Length == 0) 
             {
                 return 0;
             }
 
-            char firstLetter = word!.First();
+            char wordFirstLetter = word!.First();
             int wordLenght = word.Length;
-            int horizontalLength = content!.First().Length;
-            int verticalLength = content!.Length;
+            int wordHorizontalLength = content!.GetLength(0);
+            int wordVerticalLength = content!.GetLength(1);
+
+            coordMatrix = new char[wordHorizontalLength, wordVerticalLength];
             
-            for(int verticalPos = 0; verticalPos < content.Length; verticalPos++)
+            for(int verticalPos = 0; verticalPos < wordVerticalLength; verticalPos++)
             {
-                string line = content[verticalPos];
-                for (int horizontalPos = 0; horizontalPos < line.Length; horizontalPos++)
+                for (int horizontalPos = 0; horizontalPos < wordHorizontalLength; horizontalPos++)
                 {
-                    char letter = line[horizontalPos];
+                    char letter = content[horizontalPos,horizontalPos];
+                    if(letter == wordFirstLetter)
+                    {
+                        bool canGoUp = false, canGoDown = false, canGoLeft = false, canGoRight = false;
+
+                        if(verticalPos > wordLenght-1)
+                        {
+                            canGoUp = true;
+                        }
+                        if (horizontalPos > wordLenght - 1)
+                        {
+                            canGoLeft = true;
+                        }
+                        if (wordVerticalLength - verticalPos > wordLenght - 1)
+                        {
+                            canGoDown = true;
+                        }
+                        if (wordHorizontalLength - horizontalPos > wordLenght - 1)
+                        {
+                            canGoRight = true;
+                        }
+                    }
                 }
             }
             return 18;
         }
+
+        //private int WordFindOccurrences(int horizontalPos, int verticalPos, string word)
+        //{
+        //    content[horizontalPos][verticalPos];
+        //}
     }
 }
