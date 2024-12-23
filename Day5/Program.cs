ed2e5629182287e026456a8549582f76d79a9a61
NunoG-ND <nuno.granja@newday.co.uk>
2024-12-23 15:28:27 +0000

    day5 first commit

---
+++ Day5/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Day5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string filePath = Path.Combine(currentDirectory, "File", "input.txt");
+            string[] contents = File.ReadAllLines(filePath);
+
+            List<string> pageOrderingRules = new List<string>();
+            foreach (string line in contents.Where(t => t.Contains("|")))
+            {
+                pageOrderingRules.Add(line);
+            }
+
+            List<string> pageNumbersForUpdate = new List<string>();
+            foreach (string line in contents.Where(t => t.Contains(",")))
+            {
+                pageNumbersForUpdate.Add(line);
+            }
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
