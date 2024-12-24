799c3781b32f29a9b10cc212d7a7bd56bedc6051
Nmaster88 <nunomaster21@gmail.com>
2024-12-24 16:18:00 +0000

    fix part 1 day 5

---
+++ Day5/PrintQueue.cs
@@ -0,0 +1,56 @@
+﻿namespace Day5
+{
+    public class PrintQueue
+    {
+        public int CheckEachPageNumbersToUpdateAndCalculateSum(Dictionary<int, List<int>> pageOrderingRules, List<int[]> pageNumbersForUpdate)
+        {
+            int calculateSum = 0;
+
+            foreach (int[] numbersForUpdate in pageNumbersForUpdate)
+            {
+                bool sequenceIsValid = false;
+                for (int i = 0; i < numbersForUpdate.Length; i++)
+                {
+                    var numberToCheck = numbersForUpdate[i];
+                    sequenceIsValid = IsRulesForNumberSequenceValid(numbersForUpdate, i, pageOrderingRules);
+                    if (sequenceIsValid is not true)
+                    {
+                        break;
+                    }
+                }
+
+                if (sequenceIsValid)
+                {
+                    calculateSum += numbersForUpdate[numbersForUpdate.Length / 2];
+                }
+            }
+
+            return calculateSum;
+        }
+
+        private bool IsRulesForNumberSequenceValid(int[] numbersForUpdates, int index, Dictionary<int, List<int>> pageOrderingRules)
+        {
+            int numberToCompare = numbersForUpdates[index];
+
+            if (index < numbersForUpdates.Length - 1)
+            {
+                List<int> numberThatNeedToBeAfter = new List<int>();
+                if (pageOrderingRules.TryGetValue(numberToCompare, out var valuesRetrieved))
+                {
+                    // The key exists, and numberThatNeedToBeAfter now contains the associated List<int>.
+                    numberThatNeedToBeAfter.AddRange(valuesRetrieved);
+                }
+                for (int i = index + 1; i < numbersForUpdates.Length; i++)
+                {
+                    var numberAfter = numbersForUpdates[i];
+                    bool itIsAfter = numberThatNeedToBeAfter.Contains(numberAfter);
+                    if (itIsAfter is not true)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
--- Day5/Program.cs
+++ Day5/Program.cs
@@ -8,19 +8,24 @@
             string filePath = Path.Combine(currentDirectory, "File", "input.txt");
             string[] contents = File.ReadAllLines(filePath);
 
-            List<string> pageOrderingRules = new List<string>();
-            foreach (string line in contents.Where(t => t.Contains("|")))
-            {
-                pageOrderingRules.Add(line);
-            }
+            Dictionary<int, List<int>> pageOrderingRules = new Dictionary<int, List<int>>();
+            pageOrderingRules = contents.Where(t => t.Contains("|"))
+                .Select(entry => entry.Split('|'))
+                .GroupBy(parts => parts[0], parts => parts[1])
+                .ToDictionary(group => int.Parse(group.Key), group => group.Select(int.Parse).ToList());
 
-            List<string> pageNumbersForUpdate = new List<string>();
+            List<int[]> pageNumbersForUpdate = new List<int[]>();
             foreach (string line in contents.Where(t => t.Contains(",")))
             {
-                pageNumbersForUpdate.Add(line);
+                pageNumbersForUpdate.Add(line.Split(',').Select(int.Parse).ToArray());
             }
 
-            Console.WriteLine("Hello, World!");
+            PrintQueue printQueue = new PrintQueue();
+            int calculateSum = printQueue.CheckEachPageNumbersToUpdateAndCalculateSum(pageOrderingRules, pageNumbersForUpdate);
+
+            Console.WriteLine($"The sum of the print queue is : {calculateSum}");
+
+            Console.ReadLine();
         }
     }
 }
